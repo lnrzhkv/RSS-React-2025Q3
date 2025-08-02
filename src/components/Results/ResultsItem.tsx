861a6ca37bf24c8173f007d9aee2f3076f0cfd0c
Alina <lnrzhkv3367337@gmail.com>
2025-08-02 20:04:04 +0300

    feat: integrate SelectedItemsFlyout into PokemonList and ResultsItem copmonents

--- src/components/Results/ResultsItem.tsx
+++ src/components/Results/ResultsItem.tsx
@@ -1,17 +1,58 @@
 import React from 'react';
 import styles from './Results.module.css';
 import type { CharacterWithImage } from '../../services/api/types';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../../shared/store';
+import { addItem, removeItem } from '../../shared/selectedItemsSlice';
 
 interface Props {
   character: CharacterWithImage;
 }
 const ResultsItem: React.FC<Props> = ({ character }) => {
+  const selectedItems = useSelector((state: RootState) => state.selectedItems);
+  const dispatch = useDispatch();
+
+  const isSelected = selectedItems.some(
+    (item) => item.id === String(character.id)
+  );
+
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      dispatch(
+        addItem({
+          id: String(character.id),
+          name: character.name,
+          description: `Height: ${character.height}, Weight: ${character.weight}`,
+          detailsUrl: `/details/${character.id}`,
+        })
+      );
+    } else {
+      dispatch(removeItem(String(character.id)));
+    }
+  };
+
   return (
     <div
       className={styles.characterCard}
       data-testid={`character-card-${character.id}`}
     >
-      <div>
+      <label className={styles.toggle} onClick={(e) => e.stopPropagation()}>
+        <input
+          type="checkbox"
+          checked={isSelected}
+          onChange={handleCheckboxChange}
+          aria-label={`select-${character.name}`}
+          className={styles.toggleInput}
+        />
+        <span className={styles.slider}></span>
+      </label>
+      <div
+        style={{
+          cursor: 'pointer',
+          display: 'inline-block',
+          width: 'calc(100% - 40px)',
+        }}
+      >
         <h3 className={styles.characterName}>{character.name}</h3>
         <p className={styles.characterDetails}>
           Height: {character.height}, Weight: {character.weight}
